a490d4dfed6980294534b2f7399cbc87455b9e26
Anastasia S <schaaf.a@stu.janesville.k12.wi.us>
2022-11-23 16:05:07 -0600

    update

---
+++ mortgage-calc/js/script.js
@@ -0,0 +1,10 @@
+function domath() {
+    var P = Number(document.getElementById("princ").value);
+    var r = Number(document.getElementById("ir").value);
+    var t = Number(document.getElementById("ny").value);
+    var m = Number(document.getElementById("mp").value);
+    var n = Number(12);
+    var B = (P*Math.pow(1 + (r/n), n*t)) - ((m*((Math.pow(1 + (r/n), n*t))-1))/(r/n));
+    var p = document.getElementById("out");
+    p.innerHTML = "Remaining Loan Balance: " B;
+}
